a6f792e47b63211846f6f5c453ea8a07c5e128b0
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-07-05 12:03:14 -0400

    working on policy and terms pages

---
+++ src/pages/TermsAndConditions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TermsConditions from '../components/TermsConditions'
+import PrivacyPolicy from '../components/PrivacyPolicy'
+
+export default function TermsAndConditions() {
+  return (
+    <div className='min-h-screen '>
+      <TermsConditions />
+      {/* <PrivacyPolicy /> */}
+    </div>
+  )
+}
