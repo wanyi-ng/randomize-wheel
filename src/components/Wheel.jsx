cc57a22a1e0e78ffe2995338439699697f5cc57b
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-07-07 22:10:27 -0400

    framer motion

--- src/components/Wheel.jsx
+++ src/components/Wheel.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import RollButton from './RollButton'
 import { TrashIcon } from '@heroicons/react/24/outline'
+import { motion } from 'framer-motion'
 import { deleteWheel } from '../store/reducers/wheelSlice'
 import UpdateWheel from './UpdateWheel'
 
@@ -14,38 +15,45 @@ export default function Wheel({ wheel }) {
   }
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl h-max">
-      {/* <figure>
-        <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
-      </figure> */}
-      <div className="card-body flex flex-col justify-between space-y-8 divide-y divide-gray-500">
-        <div className='space-y-6 divide-y divide-gray-500'>
-          <div className='flex justify-between'>
-            <h2 className="card-title">{wheel.name}</h2>
-            <div className='space-x-2 '>
-              <UpdateWheel wheel={wheel} />
-              <button className='btn btn-accent text-accent-content' type='button' aria-label='Delete wheel' onClick={handleDeleteWheel}>
-                <span className='inline-flex justify-center items-center'>
-                  <TrashIcon className='w-5 h-5' />
-                  {/* Delete */}
-                </span>
-              </button>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      whileHover={{ scale: 1.05 }}
+    >
+      <div className="card w-96 bg-base-100 shadow-xl h-max drop-shadow-md">
+        {/* <figure>
+          <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+        </figure> */}
+        <div className="card-body flex flex-col justify-between space-y-8 divide-y divide-gray-500">
+          <div className='space-y-6 divide-y divide-gray-500'>
+            <div className='flex justify-between'>
+              <h2 className="card-title">{wheel.name}</h2>
+              <div className='space-x-2 '>
+                <UpdateWheel wheel={wheel} />
+                <button className='btn btn-accent text-accent-content' type='button' aria-label='Delete wheel' onClick={handleDeleteWheel}>
+                  <span className='inline-flex justify-center items-center'>
+                    <TrashIcon className='w-5 h-5' />
+                    {/* Delete */}
+                  </span>
+                </button>
+              </div>
             </div>
+            <ul className='pt-6'>
+              {wheel.options.map((option, index) => (
+                <li key={`${index}-${wheel.id}`}>{option}</li>
+              ))}
+            </ul>
           </div>
-          <ul className='pt-6'>
-            {wheel.options.map((option, index) => (
-              <li key={`${index}-${wheel.id}`}>{option}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="card-actions w-full flex flex-col items-end">
-          <RollButton wheelId={wheel.id} />
-          <div className="self-start leading-8 w-full">
-            <span>Rolled Option: </span>
-            <h2 className='font-bold text-2xl tracking-wide'>{wheel.rolledOption}</h2>
+          <div className="card-actions w-full flex flex-col items-end">
+            <RollButton wheelId={wheel.id} />
+            <div className="self-start leading-8 w-full">
+              <span>Rolled Option: </span>
+              <h2 className='font-bold text-2xl tracking-wide'>{wheel.rolledOption}</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
