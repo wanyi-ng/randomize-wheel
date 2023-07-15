c8ab4e2fdf0ba1e809344fc229223fcd08363dfa
Wanyi Ng <wanyi.ng1975@gmail.com>
2023-07-14 20:50:12 -0400

    add features demo image

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -73,7 +73,7 @@ export default function Features() {
             </div>
           </div>
           <img
-            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+            src="../images/features_demo.png"
             alt="Product screenshot"
             className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width={2432}
